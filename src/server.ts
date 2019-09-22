aeb03d1adb38546c7fb5cb0d24576173a6dcf217
John Tucker <jtucker@appirio.com>
2019-09-22 07:05:40 -0400

    env

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,17 @@
 import cors from 'cors';
 import express from 'express';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
-app.get('/', (req, res) => res.send({ hello: 'world' }));
+app.get('/', (req, res) => {
+  const hello = 'world';
+  if (process.env.DEVELOPMENT) {
+    // eslint-disable-next-line
+    console.log(hello);
+  }
+  res.send({ hello });
+});
 // eslint-disable-next-line
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));``
