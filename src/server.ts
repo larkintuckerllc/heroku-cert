2a83b3ce002e0f4efec5a1fdbd65e932bec02dfd
John Tucker <jtucker@appirio.com>
2019-09-22 21:30:56 -0400

    networking

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,20 @@
+import compression from 'compression';
 import cors from 'cors';
 import express from 'express';
+import { noCache } from 'helmet';
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
+app.use(noCache());
+app.use(compression());
 app.get('/', (req, res) => {
   const hello = 'world';
   if (process.env.DEVELOPMENT) {
+    const requestId = req.header('x-request-id');
     // eslint-disable-next-line
-    console.log(hello);
+    console.log(`${requestId}: ${hello}`);
   }
   res.send({ hello });
 });
